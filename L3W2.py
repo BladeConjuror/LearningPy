2333a9353c674db22960b431dda4a2165aaff331
BladeConjuror <146959426+BladeConjuror@users.noreply.github.com>
2026-05-10 12:14:04 +0300

    Add files via upload

---
+++ L3W2.py
@@ -0,0 +1,9 @@
+stage1 = input("Введите первый этап развития человека: ")
+stage2 = input("Введите второй этап развития человека: ")
+stage3 = input("Введите третий этап развития человека: ")
+stage4 = input("Введите четвёртый этап развития человека: ")
+stage5 = input("Введите пятый этап развития человека: ")
+stage6 = input("Введите шестой этап развития человека (последний): ")
+
+# Australopithecus / Homo habilis / Homo erectus / Homo heidelbergensis / Homo neanderthalensis / Homo sapiens sapiens
+print(stage1, stage2, stage3, stage4, stage5, stage6,sep=' => ')
